801d736580f0d68248fbd50ef2156a373b97b471
rfin786-prog <rfin786@gmail.com>
2026-05-02 05:31:26 +0600

    Remove AnimationSection from Home component

--- app/page.js
+++ app/page.js
@@ -1,16 +1,14 @@
 'use client';
 import Navbar from './components/Navbar';
-import AnimationSection from './components/AnimationSection';
 import HeroSection from './components/HeroSection';
 import FeaturesSection from './components/FeaturesSection';
 
 export default function Home() {
-  return (
-    <main style={{ minHeight: '100vh', background: '#0f2442', fontFamily: 'Hind Siliguri, sans-serif', display: 'flex', flexDirection: 'column' }}>
-      <Navbar />
-      <AnimationSection />
-      <HeroSection />
-      <FeaturesSection />
-    </main>
-  );
+ return (
+   <main style={{ minHeight: '100vh', background: '#0f2442', fontFamily: 'Hind Siliguri, sans-serif', display: 'flex', flexDirection: 'column' }}>
+     <Navbar />
+     <HeroSection />
+     <FeaturesSection />
+   </main>
+ );
 }
