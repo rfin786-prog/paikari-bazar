32da09c43940e3c2d976e4ecd740621e26c65110
rfin786-prog <rfin786@gmail.com>
2026-05-02 06:13:52 +0600

    Add ProductsSection to Home component

--- app/page.js
+++ app/page.js
@@ -2,13 +2,15 @@
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import FeaturesSection from './components/FeaturesSection';
+import ProductsSection from './components/ProductsSection';
 
 export default function Home() {
- return (
-   <main style={{ minHeight: '100vh', background: '#0f2442', fontFamily: 'Hind Siliguri, sans-serif', display: 'flex', flexDirection: 'column' }}>
-     <Navbar />
-     <HeroSection />
-     <FeaturesSection />
-   </main>
- );
+  return (
+    <main style={{ minHeight: '100vh', background: '#0f2442', fontFamily: 'Hind Siliguri, sans-serif', display: 'flex', flexDirection: 'column' }}>
+      <Navbar />
+      <HeroSection />
+      <ProductsSection />
+      <FeaturesSection />
+    </main>
+  );
 }
