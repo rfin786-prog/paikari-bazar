a80855d76fe6b559c81f45431445dbb40229b358
rfin786-prog <rfin786@gmail.com>
2026-05-11 18:25:23 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import FeaturesSection from './components/FeaturesSection';
 import ProductsSection from './components/ProductsSection';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <HeroSection />
       <ProductsSection />
       <FeaturesSection />
+      <Footer />
     </main>
   );
 }
