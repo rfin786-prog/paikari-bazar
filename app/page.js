d26f0c753edc1cd9005427013af2f00ad314b98c
rfin786-prog <rfin786@gmail.com>
2026-05-22 02:13:48 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 'use client';
 import HeroSection from './components/HeroSection';
 import CategorySection from './components/CategorySection';
+import BrandsSection from './components/BrandsSection';
 import ProductsSection from './components/ProductsSection';
 import CTABanner from './components/CTABanner';
 import Footer from './components/Footer';
@@ -10,6 +11,7 @@ export default function Home() {
     <main style={{ minHeight: '100vh', background: '#f5f5f5', fontFamily: 'Hind Siliguri, sans-serif', display: 'flex', flexDirection: 'column' }}>
       <HeroSection />
       <CategorySection />
+      <BrandsSection />
       <ProductsSection />
       <CTABanner />
       <Footer />
